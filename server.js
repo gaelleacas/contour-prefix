6838463fcfa803c7492e1843c96a701ca76cf723
gaelleiadvize <gaelle.acas@iadvize.com>
2019-03-14 09:39:15 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.status(200).send('Hello World from root');
+});
+
+app.get('/myroute', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.status(200).send('Hello World from route /myroute');
+});
+app.listen(process.env.PORT || 8080);
